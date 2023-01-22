3bb4febb13fe6507a3c50f7818e3d31c2ff5fcff
Dhanusritk <107863345+Dhanusritk@users.noreply.github.com>
2023-01-22 19:24:50 +0530

    Create Subtract-the-Product-and-Sum-of-Digits-of-an-Integer.cpp

---
+++ Subtract-the-Product-and-Sum-of-Digits-of-an-Integer.cpp
@@ -0,0 +1,24 @@
+using namespace std;
+class Solution {
+public:
+    int subtractProductAndSum(int n) {
+        int prod=1;
+        int sum=0;
+        while(n>0){
+            int rem= n%10;
+            prod*=rem;
+            sum+=rem;
+            n=n/10;
+        }
+        int res= prod-sum;
+        return res;
+        
+    }
+};
+int main(){
+    Solution s;
+    int n=234;
+    int x= s. subtractProductAndSum(n);
+    cout<<x<<endl;
+    return 0;
+}
